84158469b8062e963840bc1d0a8588b2eafac84d
Allison Patterson <allison.patterson@students.olin.edu>
2015-03-01 20:24:16 -0500

    took out the whoopsie res.send bit in main

--- routes/main.js
+++ routes/main.js
@@ -13,12 +13,11 @@ module.exports.list = function (req, res) {
 
 module.exports.article = function (req, res) {
 	var rq = req.params;
-	info = res.json({
-		   articleTitle: rq.name + "!",
-		   articleContent: rq.name +
-		   " is/are a sublime aquatic mammal of great size."
-	       });
-	res.send("index",{info: info});
+	res.json({
+	   articleTitle: rq.name + "!",
+	   articleContent: rq.name +
+	   " is/are a sublime aquatic mammal of great size."
+       });
 };
 
 module.exports.edit = function (req, res) {
