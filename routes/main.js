eb559c86ab831bd01403a09a4d89892fa5a4ab7f
Allison Patterson <allison.patterson@students.olin.edu>
2015-03-01 20:44:02 -0500

    rigged up articles handler to database

--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,5 @@
+var Article = require("../model/article")
+
 module.exports.index = function (req, res) {
 	res.render('index', {
 		articles: [
@@ -8,16 +10,16 @@ module.exports.index = function (req, res) {
 }
 
 module.exports.list = function (req, res) {
-	articles.find()
+
 }
 
 module.exports.article = function (req, res) {
 	var rq = req.params;
-	res.json({
-	   articleTitle: rq.name + "!",
-	   articleContent: rq.name +
-	   " is/are a sublime aquatic mammal of great size."
-       });
+	Article.findOne({title: rq.title}, function (err,article) {
+	   if (err) return res.sendStatus(500);
+	   console.log(article);
+	   res.json(article);
+	});
 };
 
 module.exports.edit = function (req, res) {
